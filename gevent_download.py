9f887aedd6e7c120cffbd98f8f2102d82c4d2047
snnxyss <96976810+snnxyss@users.noreply.github.com>
2022-01-14 22:43:46 +0800

    Update gevent_download.py

--- gevent_download.py
+++ gevent_download.py
@@ -1,5 +1,5 @@
-import time
-import random
+
+import threading
 import os
 import multiprocessing
 from url_mange import Urlmanage
@@ -9,47 +9,45 @@ import requests
 import re
 class gevent(object):
 #     #代理池
-    def get_proxy(self):
-        result = []
-        # 读取代理池ip并随机返回一个
-        with open('ip_pool_home.txt', 'r') as f:
-            for line in f:
-                result.append(line.strip('\n'))
-        ips = list(filter(None, result))
-        proxies = {'http': random.choice(ips)}
-        return proxies
+#     def get_proxy(self):
+#         result = []
+#         # 读取代理池ip并随机返回一个
+#         with open('ip_pool_home.txt', 'r') as f:
+#             for line in f:
+#                 result.append(line.strip('\n'))
+#         ips = list(filter(None, result))
+#         proxies = {'http': random.choice(ips)}
+#         return proxies
 
 
 
-    def pro(self,url,uid,pid):
+    def pro(self,url,uid):
         os.mkdir('/Users/' + uid)
-        proxies=gevent().get_proxy()
+        print("url22222===%s"%url)
+        # proxies=gevent().get_proxy()
         str=[]
         str2=[]
         urls=[]
         names=[]
         headers2=Urlmanage().head2()
         headers=Urlmanage().head1()
-        res = requests.get(url, headers=headers,proxies=proxies)
+        res = requests.get(url, headers=headers)
+        print("res===%s"%res)
         list = res.json()
         # print(list)
         # json.dumps(list,sort_keys=True,indent=2)
         for l in list:
-            try:
-                l = list['body']['works']
-            except Exception:
-                continue
+            l = list['body']['works']
 
         # print(l)
         # print(l.keys)
 
         for i in l:
-            try:
-                urls.append(l[i]['url'])
 
-                names.append(l[i]['title'])
-            except Exception:
-                continue
+            urls.append(l[i]['url'])
+
+            names.append(l[i]['title'])
+
         # print(urls)
         for a in urls:
             str.append(re.findall('img/(.*?)_p0', a))
@@ -66,36 +64,35 @@ class gevent(object):
             print("string2==%s" % string2)
             headers2['referer'] = string2  # 将headers的referer更新为拼接好的链接地址，进行访问
             print(url_img.format(astr[0]))
-            rrr = requests.get(url_img.format(astr[0]), headers=headers2,proxies=proxies)
-            rrr2 = requests.get(url_img2.format(astr[0]), headers=headers2,proxies=proxies)
-            try:
-                if rrr.status_code == 200:
-                    with open('/Users/%s/%s.png' % (uid, i), 'wb+') as f:  ##默认的村塾路径
-                        # if rrr.status_code==200:
+            rrr = requests.get(url_img.format(astr[0]), headers=headers2)
+            rrr2 = requests.get(url_img2.format(astr[0]), headers=headers2)
+            if rrr.status_code == 200:
+                with open('/Users/%s/%s.png' % (uid, i), 'wb+') as f:  ##默认的存储路径
+                    # if rrr.status_code==200:
 
-                        f.write(rrr.content)
+                    f.write(rrr.content)
 
-                        print("%s下载成功\n" % names[i])
+                    print("%s下载成功\n" % names[i])
 
-                else:
-                    with open('/Users/%s/%s.png' % (uid, i), 'wb+') as p:
-                        p.write(rrr2.content)
-                        print("%s下载成功\n" % names[i])
-                i += 1
-                time.sleep(0.3)
-            except Exception:
-                continue
-
+            else:
+                with open('/Users/%s/%s.png' % (uid, i), 'wb+') as p:
+                    p.write(rrr2.content)
+                    print("%s下载成功\n" % names[i])
+            i += 1
 if __name__ == '__main__':
     date= input("请输入你的主页id: ")
-    uids = get_uid().userid(data=date)
-    print(uids)
-    pool = multiprocessing.Pool(processes=4)
-    # print(uids)
+    qua=input("请输入爬取的用户数: ")
+    uids = get_uid().userid(data=date,qua=qua)
+    print("uids===%s"%uids)
+    pool = multiprocessing.Pool(processes=4) #进程池
     for uid in uids:
         pids = get_pid().getpid(uid=uid)
         url = 'https://www.pixiv.net/ajax/user/{uid}/profile/illusts?{pid}&work_category=illustManga&is_first_page=1&lang=zh'.format(
             uid=uid, pid=pids)
-        pool.apply_async(gevent().pro, (url, uid, pids,))
+        # print("url===%s"%url)
+        print("uid===%s"%uid)
+        # print("pids===%s"%pids)
+        pool.apply_async(gevent().pro, (url, uid))
+
     pool.close()
     pool.join()
